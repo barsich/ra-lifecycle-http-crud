995b0a9b0dd01d53c383a1c515ad24cce2f9b9c6
barsich <barsicchan@gmail.com>
2021-11-02 19:14:20 +0500

    some fixes

--- src/components/Notes/NotesForm/index.js
+++ src/components/Notes/NotesForm/index.js
@@ -1,9 +1,18 @@
-export default function NotesForm({ onSubmit, onChange, onUpdate }) {
+export default function NotesForm({ onSubmit, onChange, onUpdate, text }) {
   return (
     <form className="notes-form" onSubmit={onSubmit}>
-      <textarea rows="5" className="notes-form__textarea" onChange={onChange}></textarea>
-      <button className="notes-form__submit button" type="submit">Добавить</button>
-      <button className="notes-form__update button" type="button" onClick={onUpdate}>Обновить</button>
+      <textarea
+        rows="5"
+        className="notes-form__textarea"
+        onChange={onChange}
+        value={text}
+      ></textarea>
+      <button className="notes-form__submit button" type="submit">
+        Добавить
+      </button>
+      <button className="notes-form__update button" type="button" onClick={onUpdate}>
+        Обновить
+      </button>
     </form>
   );
 }
